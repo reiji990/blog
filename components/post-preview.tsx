f32442d92cc876c2f0db056dc282463513592761
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-05-08 20:58:11 +0900

    デザイン・レイアウトの変更
    [参考1](https://zenn.dev/miketako3/articles/66e1cc17193168)
    
    [参考2](https://zenn.dev/miketako3/articles/2afb29824e578d)

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -22,24 +22,22 @@ const PostPreview = ({
   slug,
 }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
+    <div className="flex flex-row">
+      <div className="w-32 h-32">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
-          {title}
-        </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
+      <div className="flex-1 ml-4">
+        <h3 className="text-xl">
+          <Link as={`/posts/${slug}`} href="/posts/[slug]">
+          </Link>
+        </h3>
+        <div className="flex flex-row">
+          <Avatar name={author.name} picture={author.picture} />
+          <div className="font-bold text-xs my-auto mx-2">
+            <DateFormatter dateString={date} />
+          </div>
+        </div>
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }

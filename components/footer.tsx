99ae122d7782158d01bbe680245203560bcf9abf
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-05-13 21:53:52 +0900

    ブログテンプレート変更

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,32 +1,22 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
-import React from 'react';
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer className="border-t ">
-      <Container>
-        <div className="pt-14 flex justify-center  ">
-            <a
-              href={`https://twitter.com/reiji990/`}
-              className="mx-3 font-bold hover:underline"
-            >
-            <img src="/social-icons/twitter.svg" width="30" />
-            </a>
-            <a
-              href={`https://github.com/reiji990/`}
-              className="mx-3 font-bold hover:underline"
-            >
-            <img src="/social-icons/github.svg" width="30" />
-            </a>
-
-          </div>
-        <div className="pt-5 pb-14 flex justify-center">
-          ©️ 2023 reiji990
+    <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
         </div>
-      </Container>
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <Link href="/">{siteMetadata.author}</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        </div>
+      </div>
     </footer>
   )
 }
-
-export default Footer
--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,45 @@
-import Link from 'next/link'
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 ">
-      <Link href="/" className="hover:underline">
-        Web log.(仮)
-      </Link>
-      
-    </h2>
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+            </div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="hidden h-6 text-2xl font-semibold sm:block">
+                Home
+              </div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center text-base leading-5">
+        <div className="hidden sm:block">
+          {headerNavLinks.map((link) => (
+            
+            <Link
+              key={link.title}
+              href={link.href}
+              className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
   )
 }
 

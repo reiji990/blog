f6615de420901b13e35b2f7601bf07c9b40eee28
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-05-15 23:13:50 +0900

    微修正

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                Home
-              </div>
+              <div className="hidden h-6 text-2xl font-semibold sm:block">Home</div>
             ) : (
               siteMetadata.headerTitle
             )}
@@ -22,7 +20,7 @@ const Header = () => {
       </div>
       <div className="flex items-center text-base leading-5">
         <div className="hidden sm:block">
-          {headerNavLinks.map((link) => (            
+          {headerNavLinks.map((link) => (
             <Link
               key={link.title}
               href={link.href}

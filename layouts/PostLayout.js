991c1b6fe0e1a23218201955c0181b4435d00ccb
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-05-18 08:53:17 +0900

    error修正

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -15,7 +15,8 @@ const postDateTemplate = {
 export default function PostLayout({ content, authorDetails, next, prev, children }) {
   const { filePath, path, date, title, tags } = content
   const shareUrl = (path) =>
-    `https://twitter.com/intent/tweet?text=${title}｜${siteMetadata.title}%20${siteMetadata.siteUrl + path}%20@${siteMetadata.author}`
+    `https://twitter.com/intent/tweet?text=${title}｜${siteMetadata.title}%20${siteMetadata.siteUrl + path
+    }%20@${siteMetadata.author}`
   const editUrl = (path) => `${siteMetadata.siteRepo}/blob/master/data/${path}`
   const basePath = path.split('/')[0]
   return (
@@ -80,7 +81,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={shareUrl(path)} rel="nofollow">Tweet</Link>
+                <Link href={shareUrl(path)} rel="nofollow">
+                  Tweet
+                </Link>
                 {` • `}
                 <Link href={editUrl(filePath)}>View on GitHub</Link>
               </div>

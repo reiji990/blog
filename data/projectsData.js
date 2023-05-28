11b1b106606eadea0213d148c90da648a26eaaa5
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-05-28 14:45:32 +0900

    Projects追加

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,3 +1,9 @@
 const projectsData = [
+    {
+        title: 'reiji990/blog',
+        description: ``,
+        imgSrc: '/static/images/GitHub_Logo.png',
+        href: 'https://github.com/reiji990/blog',
+      },
 ]
 export default projectsData

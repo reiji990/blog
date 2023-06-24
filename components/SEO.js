52877b57c5cd180b6315f321bc2d1c578005e00b
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-06-24 23:16:47 +0900

    リンクカードにサブタイトル追加

--- components/SEO.js
+++ components/SEO.js
@@ -6,16 +6,14 @@ const CommonSEO = ({ title, subtitle, description, ogType, ogImage, twImage, can
   const router = useRouter()
   return (
     <Head>
-      <title>
-        '${title} ${subtitle}'
-      </title>
+      <title>{title}</title>
       <meta name="robots" content="follow, index" />
       <meta name="description" content={description} />
       <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
       <meta property="og:type" content={ogType} />
-      <meta property="og:site_name" content={siteMetadata.title} />
+      <meta property="og:site_name" content={'${siteMetadata.title} ${siteMetadata.subtitle}'} />
       <meta property="og:description" content={description} />
-      <meta property="og:title" content={`${title} ${subtitle}`} />
+      <meta property="og:title" content={title} />
       {ogImage.constructor.name === 'Array' ? (
         ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
       ) : (
@@ -23,7 +21,7 @@ const CommonSEO = ({ title, subtitle, description, ogType, ogImage, twImage, can
       )}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={siteMetadata.twitter} />
-      <meta name="twitter:title" content={`${title} ${subtitle}`} />
+      <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={twImage} />
       <link

88838645496fb366cb8d612a85d9e2ed30a3a819
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-08-21 09:51:17 +0900

    Apply new templete.
    https://tailwind-nextjs-starter-blog.vercel.app/blog/release-of-tailwind-nextjs-starter-blog-v2.0

--- data/projectsData.js
+++
@@ -1,9 +0,0 @@
-const projectsData = [
-  {
-    title: 'reiji990/blog',
-    description: 'このブログのリポジトリです。',
-    imgSrc: '/static/images/GitHub_Logo.png',
-    href: 'https://github.com/reiji990/blog',
-  },
-]
-export default projectsData

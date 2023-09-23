9fb99ec9f20b3fb45426512290f3534afff25803
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-09-24 01:28:56 +0900

    error修正

--- components/Spotify.tsx
+++ components/Spotify.tsx
@@ -3,15 +3,14 @@ interface Props {
 }
 
 export default function SpotifyPlayer({ id }: Props) {
-
   return (
-    <iframe
-      style={{ borderRadius: '12px' }}
-        src={`https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`}
-        width="100%"
-        height="152"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-    ></iframe>
-  )
+<iframe
+  style={{ borderRadius: '12px' }}
+    src={`https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`}
+    width="100%"
+    height="152"
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    loading="lazy"
+   ></iframe>
+ )
 }

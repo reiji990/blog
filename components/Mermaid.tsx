67f5bd97e9108421a4c6ec20573411d97462bed8
reiji990 <101491438+reiji990@users.noreply.github.com>
2024-09-08 23:49:34 +0900

    Mermaid.js、Dnut(chart.js)、Line(chartljs)追加

---
+++ components/Mermaid.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useEffect, useState } from 'react'
+import mermaid from 'mermaid'
+
+const Mermaid = ({ chart }) => {
+  const [theme, setTheme] = useState('default')
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+    const handleThemeChange = (e) => {
+      setTheme(e.matches ? 'dark' : 'default')
+    }
+
+    setTheme(darkModeMediaQuery.matches ? 'dark' : 'default')
+    darkModeMediaQuery.addEventListener('change', handleThemeChange)
+
+    return () => {
+      darkModeMediaQuery.removeEventListener('change', handleThemeChange)
+    }
+  }, [])
+
+  useEffect(() => {
+    mermaid.initialize({ theme })
+    mermaid.contentLoaded()
+  }, [theme, chart])
+
+  return <div className="mermaid">{chart}</div>
+}
+
+export default Mermaid

9143a36be9cc46cb71a4827674aa598fd1acb092
reiji990 <101491438+reiji990@users.noreply.github.com>
2024-09-09 00:00:22 +0900

    修正

--- components/Mermaid.tsx
+++ components/Mermaid.tsx
@@ -1,29 +1,12 @@
 'use client'
-import { useEffect, useState } from 'react'
 import mermaid from 'mermaid'
+import { useEffect } from 'react'
 
 const Mermaid = ({ chart }) => {
-  const [theme, setTheme] = useState('default')
-
   useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-    const handleThemeChange = (e) => {
-      setTheme(e.matches ? 'dark' : 'default')
-    }
-
-    setTheme(darkModeMediaQuery.matches ? 'dark' : 'default')
-    darkModeMediaQuery.addEventListener('change', handleThemeChange)
-
-    return () => {
-      darkModeMediaQuery.removeEventListener('change', handleThemeChange)
-    }
-  }, [])
-
-  useEffect(() => {
-    mermaid.initialize({ theme })
+    mermaid.initialize({ startOnLoad: true })
     mermaid.contentLoaded()
-  }, [theme, chart])
+  }, [chart])
 
   return <div className="mermaid">{chart}</div>
 }

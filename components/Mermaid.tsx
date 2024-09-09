d5ee547497fa142c53fe67ed8e2c9b4cfe1979a1
reiji990 <101491438+reiji990@users.noreply.github.com>
2024-09-09 11:01:07 +0900

    修正

--- components/Mermaid.tsx
+++ components/Mermaid.tsx
@@ -4,13 +4,13 @@ import mermaid from 'mermaid'
 import { useTheme } from 'next-themes'
 
 const Mermaid = ({ chart }) => {
-  const { theme } = useTheme()  // ThemeProviderからテーマを取得
+  const { theme } = useTheme() // ThemeProviderからテーマを取得
 
   useEffect(() => {
-    const mermaidTheme = theme === 'dark' ? 'dark' : 'default'  // テーマに基づいてmermaidのテーマを選択
+    const mermaidTheme = theme === 'dark' ? 'dark' : 'default' // テーマに基づいてmermaidのテーマを選択
     mermaid.initialize({ theme: mermaidTheme })
     mermaid.contentLoaded()
-  }, [theme, chart])  // テーマやチャートが変更されたら再レンダリング
+  }, [theme, chart]) // テーマやチャートが変更されたら再レンダリング
 
   return <div className="mermaid">{chart}</div>
 }

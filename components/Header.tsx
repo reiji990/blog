b652461706b526114005ac90954f2abd93095f62
reiji990 <101491438+reiji990@users.noreply.github.com>
2024-11-13 01:21:54 +0900

    error修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,24 +14,11 @@ const Header = () => {
 
   return (
     <header className={headerClass}>
-      <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
-          </div>
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
-          ) : (
-            siteMetadata.headerTitle
-          )}
-        </div>
-      </Link>
+        <div></div>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
           {headerNavLinks
-            .filter((link) => link.href !== '/')
+            .filter((link) => link.href !== '')
             .map((link) => (
               <Link
                 key={link.title}

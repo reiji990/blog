0234ed703d51e165fa071caaa8e772fa20839841
reiji990 <101491438+reiji990@users.noreply.github.com>
2025-04-03 00:32:41 +0900

    error修正

--- components/PageSubTitle.tsx
+++ components/PageSubTitle.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function PageSubTitle({ children }: Props) {
   return (
-    <h2 className="text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:leading-14">
+    <h2 className="text-3xl leading-9 tracking-tight text-gray-900 sm:leading-10 md:leading-14 dark:text-gray-100">
       {children}
     </h2>
   )

a6cf0ed8d43e0566e62ce8fcbce7a360a05ec05e
reiji990 <101491438+reiji990@users.noreply.github.com>
2025-04-03 00:46:51 +0900

    error修正

--- layouts/PostBanner.tsx
+++ layouts/PostBanner.tsx
@@ -23,8 +23,8 @@ const postDateTemplate: Intl.DateTimeFormatOptions = {
 interface LayoutProps {
   content: CoreContent<Blog>
   children: ReactNode
-  next?: { path: string; title: string; subtitle: string; draft: boolean  }
-  prev?: { path: string; title: string; subtitle: string; draft: boolean  }
+  next?: { path: string; title: string; subtitle: string; draft: boolean }
+  prev?: { path: string; title: string; subtitle: string; draft: boolean }
 }
 
 export default function PostMinimal({ content, next, prev, children }: LayoutProps) {
@@ -46,7 +46,7 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
                 </div>
               </Bleed>
               <div>
-                <dd className="pt-6 text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                <dd className="pt-6 text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
                   <time dateTime={date}>
                     {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
                   </time>
@@ -63,21 +63,21 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
               <PageTitle>{title}</PageTitle>
               <PageSubTitle>{subtitle}</PageSubTitle>
             </div>
-            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+            <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
               {summary}
             </dd>
           </div>
-          <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-            <div className="prose max-w-none py-4 dark:prose-invert">{children}</div>
-            <div className="pb-6 pt-6 text-center text-sm text-gray-700 dark:text-gray-300">
+          <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
+            <div className="prose dark:prose-invert max-w-none py-4">{children}</div>
+            <div className="pt-6 pb-6 text-center text-sm text-gray-700 dark:text-gray-300">
               <Link href={editUrl(filePath)}>View on GitHub</Link>
               <Share title={title} subtitle={subtitle} slug={slug} summary={summary} />
             </div>
-          {siteMetadata.comments && (
-            <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300" id="comment">
-              <Comments slug={slug} />
-            </div>
-          )}
+            {siteMetadata.comments && (
+              <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300" id="comment">
+                <Comments slug={slug} />
+                </div>
+            )}
             <div className="justify-between py-4 xl:block">
               <Link
                 href={`/${basePath}`}
@@ -87,41 +87,41 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
                 &larr; Back to the blog
               </Link>
             </div>
-          <footer>
-            <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
+            <footer>
+              <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
                 {(next || prev) && (
                   <div className="justify-between py-4 xl:block">
                     {prev && prev.draft === false && (
                       <div>
-                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                        <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                           Previous Article
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/${prev.path}`}>
                             {prev.title} {prev.subtitle}
-                  </Link>
-                        </div>
-                </div>
-              )}
+                        </Link>
+                   </div>
+                      </div>
+                    )}
                     {next && next.draft === false && (
                       <div className="justify-between py-4 xl:block">
-                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                        <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                           Next Article
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/${next.path}`}>
                             {next.title} {next.subtitle}
-                  </Link>
+                        </Link>
                         </div>
                       </div>
                     )}
                   </div>
                 )}
               </div>
-              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+              <div className="divide-gray-200 text-sm leading-5 font-medium xl:col-start-1 xl:row-start-2 xl:divide-y dark:divide-gray-700">
                 {tags && (
                   <div className="py-4 xl:py-8">
-                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                       Tags
                     </h2>
                     <div className="flex flex-wrap">
@@ -129,10 +129,10 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
                         <Tag key={tag} text={tag} />
                       ))}
                     </div>
-                </div>
-              )}
-            </div>
-          </footer>
+                  </div>
+                )}
+              </div>
+            </footer>
           </div>
         </div>
       </article>

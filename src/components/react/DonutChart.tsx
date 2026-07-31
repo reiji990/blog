0e76f3582e761e66141fad0d7435ae749ea46c7e
reiji990 <101491438+reiji990@users.noreply.github.com>
2026-08-01 02:14:12 +0900

    グラフ表示不具合修正

--- src/components/react/DonutChart.tsx
+++ src/components/react/DonutChart.tsx
@@ -6,7 +6,10 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js'
 ChartJS.register(ArcElement, Tooltip, Legend, Title)
 
 const DoughnutChart = ({ data, options }) => {
-  return <Doughnut data={data} options={options} />
+  // ラッパ (DonutChart.astro の h-64) の高さいっぱいに描く。既定の
+  // maintainAspectRatio: true だと幅から高さを決めてラッパをはみ出し、
+  // 高さ固定の親との間でリサイズが収束しない
+  return <Doughnut data={data} options={{ maintainAspectRatio: false, ...options }} />
 }
 
 export default DoughnutChart
